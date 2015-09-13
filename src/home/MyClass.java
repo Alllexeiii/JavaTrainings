e202844186f9dc50a66e95a16098f56090d9f3b1
Alllexeiii <savchenkoalexey@yandex.ru>
2015-09-13 18:26:27 +0300

    w

--- src/home/MyClass.java
+++ src/home/MyClass.java
@@ -1,12 +1,43 @@
 package home;
 
-/**
- * Created by User on 06.09.2015.
- */
 public class MyClass {
 
     public static void main(String[] args) {
-        System.out.println("hello world!!!");
-    }
+        //Массивы
+        int array[] = new int[5];//создаем массив
+        for (int i = 0; i < array.length; i++) {
+            array[i] = i;
+            System.out.println(array[i]);
+        }
+        for(int v: array) { //foreach
+            System.out.println(v);
+        }
 
+        int w =1;
+        switch (w) {
+            case 1:
+                System.out.println("1");
+                break;
+            case 2:
+                System.out.println("2");
+                break;
+            default:
+                System.out.println("0");
+                break;
+        }
+
+        //String
+        String f = "Java";
+        String f1 = new String("Java");
+        System.out.println(f);
+        System.out.println(f1);
+        System.out.println(f.equals(f1));//сравниваем строки по значению
+        String f3 = f.replace("J", "G");
+
+        String s = "";
+        for(int i = 1; i < 11; i++) {
+            s = s + i + ";";
+        }
+        System.out.println(s);
+    }
 }
